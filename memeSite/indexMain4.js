4b14d14031308460fda8e915e76283390ae7ed3e
bluebasecasinoBot <developerme33@gmail.com>
2024-10-30 20:10:08 +0100

    Update indexMain4.js

--- memeSite/indexMain4.js
+++ memeSite/indexMain4.js
@@ -44,7 +44,7 @@ __SYD.roadMapPage = () =>{
                         },
                         [
                             `
-                                Stake
+                                Skate
                             `
                         ]
                     ),
